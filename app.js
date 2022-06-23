21b911d458fb944a1d90a9305c1867b043c19234
Kartik Raina <96180683+KARTIK-RAINA5802@users.noreply.github.com>
2022-06-23 15:29:13 +0530

    Removing array items and installing mongoose to use mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 
-var items = [];
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
